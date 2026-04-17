098a87492a632f030bbb71e627fc94432f1570a8
Mohammad Fazil Anwaz <fazilanwaz124@gmail.com@gmail.com>
2026-04-17 11:12:16 +0530

    Supply Chain Risk Profiling Project

---
+++ .ipynb_checkpoints/risk_profiling-checkpoint.py
@@ -0,0 +1,13 @@
+# ============================================================
+# PRJN26-250 | Data-Driven Risk Profiling for Supply Chain
+# Cell 1: Import LibrariesS
+# ============================================================
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+print("✅ All libraries loaded successfully!")
+print(f"Pandas version: {pd.__version__}")
+print(f"NumPy version: {np.__version__}")
